84652bc8f844570be41cb7d4180528a55ceb3614
j-palomino <jpalomino09@gmail.com>
2024-10-19 00:21:40 -0700

    adds rootfun contract

---
+++ foundry/script/RootFun.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/RootFun.sol";
+
+contract DeployRootFun is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        RootFun rootFun = new RootFun();
+
+        vm.stopBroadcast();
+
+        console.log("RootFun deployed at:", address(rootFun));
+    }
+}

2c6c24588e91f652490c6cb4f00d92388291a781
Erik Faz <43554741+shadowfaz16@users.noreply.github.com>
2024-10-20 07:36:34 -0700

    fixed build

--- rootstockfun/src/app/api/walruslinks/route.ts
+++ rootstockfun/src/app/api/walruslinks/route.ts
@@ -1,71 +1,71 @@
 import { NextResponse } from 'next/server';
 
-export async function generateLink() {
-    try {
-      const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin': '*'},
-        body: '{"name":"MoonDoge","symbol":"MDOGE","img":"https://example.com/moondoge.png","description":"MoonDoge is the next generation meme coin, combining the lovable Shiba Inu meme with the dream of going to the moon. Our community-driven project aims to revolutionize the crypto space with fun, engagement, and potential astronomical gains. Join the MoonDoge pack and prepare for liftoff!"}'
-};
+// export async function generateLink() {
+//     try {
+//       const options = {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' ,'Access-Control-Allow-Origin': '*'},
+//         body: '{"name":"MoonDoge","symbol":"MDOGE","img":"https://example.com/moondoge.png","description":"MoonDoge is the next generation meme coin, combining the lovable Shiba Inu meme with the dream of going to the moon. Our community-driven project aims to revolutionize the crypto space with fun, engagement, and potential astronomical gains. Join the MoonDoge pack and prepare for liftoff!"}'
+// };
   
-      const response = await fetch('https://wapo-testnet.phala.network/ipfs/QmdeYyPzw9c3WHBdd9nmTrUHqQ666YPefbPxhtQCM5WvS7', options);
+//       const response = await fetch('https://wapo-testnet.phala.network/ipfs/QmdeYyPzw9c3WHBdd9nmTrUHqQ666YPefbPxhtQCM5WvS7', options);
 
-      console.log("response: ", response);
+//       console.log("response: ", response);
   
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//       }
   
-      const data = await response.json();
-      console.log("data: ", data);
-      return data;
-    } catch (error) {
-      console.error('Error:', error);
-      throw error;
-    }
-  }
+//       const data = await response.json();
+//       console.log("data: ", data);
+//       return data;
+//     } catch (error) {
+//       console.error('Error:', error);
+//       throw error;
+//     }
+//   }
 
-export async function getFillerItems() {
-  try {
-    const response = await fetch('https://wapo-testnet.phala.network/ipfs/QmePfWmPzbtb6mhiTXynrAV3f3tTVTkM2p1cqLbmAwf8Ge');
+// export async function getFillerItems() {
+//   try {
+//     const response = await fetch('https://wapo-testnet.phala.network/ipfs/QmePfWmPzbtb6mhiTXynrAV3f3tTVTkM2p1cqLbmAwf8Ge');
     
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching filler items:', error);
-    throw error;
-  }
-}
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error('Error fetching filler items:', error);
+//     throw error;
+//   }
+// }
 
-export async function getHoldersByAddress(tokenAddress: string) {
-  try {
-    const baseUrl = 'https://instance_base_url/api';
-    const params = new URLSearchParams({
-      module: 'token',
-      action: 'getTokenHolders',
-      contractaddress: tokenAddress,
-      page: '1',
-      offset: '100'
-    });
+// export async function getHoldersByAddress(tokenAddress: string) {
+//   try {
+//     const baseUrl = 'https://instance_base_url/api';
+//     const params = new URLSearchParams({
+//       module: 'token',
+//       action: 'getTokenHolders',
+//       contractaddress: tokenAddress,
+//       page: '1',
+//       offset: '100'
+//     });
 
-    const url = `${baseUrl}?${params.toString()}`;
-    const response = await fetch(url);
+//     const url = `${baseUrl}?${params.toString()}`;
+//     const response = await fetch(url);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching token holders:', error);
-    throw error;
-  }
-}
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error('Error fetching token holders:', error);
+//     throw error;
+//   }
+// }
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
